e6b8122f099f352971ad5ce8a92d7b29df222f4c
Barvik <petter.barvik@gmail.com>
2015-10-06 18:24:30 +0200

    Add Treatment and Javascript

---
+++ MuscleTherapyJournal/Controllers/ImageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace MuscleTherapyJournal.Controllers
+{
+    public class ImageController : Controller
+    {
+        
+        [HttpGet]
+        public ActionResult GetBase64Image()
+        {
+            string path 
+                = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images")
+                 + "\\" + "Treatment.jpg";
+ 
+            FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            byte[] data = new byte[(int)fileStream.Length];
+            fileStream.Read(data, 0, data.Length);
+ 
+            return Json(new { base64imgage =  Convert.ToBase64String(data) }
+                , JsonRequestBehavior.AllowGet);
+        } 
+    }
+}
---
+++ MuscleTherapyJournal/Gruntfile.js
@@ -0,0 +1,40 @@
+﻿/*global module:false*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            development: {
+                options: {
+                    paths: ["components"],
+                    ieCompat: true,
+                    relativeUrls: true
+                },
+                files: {
+                    /*Just moving the MDS CSS files we got now, when we compile the less ourself it doesn't work. */
+                    "Content/treatment.css": "less/treatment.less"
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['less/*.less', 'less/*/*.less', 'mdx/less/*.less', 'components/**/*.less'],
+                tasks: ['less'],
+                options: {
+                    spawn: false,
+                    livereload: true
+                }
+            }/*,
+    gruntfile: {
+      files: '<%= jshint.gruntfile.src %>',
+      tasks: ['jshint:gruntfile']
+    }*/
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', ['watch']);
+};
